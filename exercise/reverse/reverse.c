cf560a3459826e864dd7a8b74bb5c4040a25d6c6
shahinjowkar <shahin.jowkardris@mail.mcgill.ca>
2025-10-02 00:18:58 -0400

    basic reverse done

--- exercise/reverse/reverse.c
+++ exercise/reverse/reverse.c
@@ -21,45 +21,80 @@ addToEnd(node *root, char *val){
 }
 
 void
-printNode(node *root){
+printNode(node *root, FILE *output){
     node *dummy = root;
     while(dummy){
         if(dummy != root){
-            printf("->%s",dummy->val);
+            fprintf(output,"%s",dummy->val);
         }
         dummy = dummy->next;
     }
-    printf("\n");
+    fprintf(output, "\n");
 }
 
-void *
+void
 reverse(node* root){
-    node *prev = root;
+    node *prev = NULL;
     node *curr = root->next;
     while(curr){
         node *dummy = curr->next;
-        // printf("%s\n",dummy->val);
         curr->next = prev;
-        // printf("%s\n",curr->next->val);
         prev = curr;
-        // printf("%s\n",prev->val);
         curr = dummy;
-        // printf("%s\n",curr->val);
     }
-    return curr;
+    root->next = prev;
 }
 
 int
 main(int argc, char *argv[]){
-    node *root = malloc(sizeof(node));
-    root->val = NULL;
-    root->next = NULL;
-    addToEnd(root, "node1");
-    addToEnd(root, "node2");
-    addToEnd(root, "node3");
-    printNode(root);
-    root = reverse(root);
-    printf("%s\n",root->next->val);
-    printNode(root);
 
+    FILE *input;
+    FILE *output;
+    
+    if(argc > 3){
+        fprintf(stderr, "Too many arguments");
+        exit(1);
+    }
+    
+    else if(argc == 1){
+        input = stdin;
+        output = stdout;
+    }
+    
+    else if(argc == 3){
+        input = fopen(argv[1],"r");
+        if(!input){
+            fprintf(stderr,"file does not exists!");
+            fclose(input);
+            exit(1);
+        }
+        output = fopen(argv[2], "w");
+    }
+    else{
+        fprintf(stderr, "missing argument");
+        exit(1);
+    }
+    
+    node *root = malloc(sizeof(node));
+    root->next = NULL;
+    root->val = NULL;
+    char *buff = NULL;
+    size_t size = 0;
+    
+    while(getline(&buff,&size,input) != -1){
+        if(strcmp(buff,"0\n") == 0){
+            break;
+        }
+        addToEnd(root,buff);
+    }
+    
+    free(buff);
+    reverse(root);
+    printNode(root, output);
+    
+    if (input  && input  != stdin)  fclose(input);
+    if (output && output != stdout) fclose(output);
+    
+    return 0;
+    
 }
